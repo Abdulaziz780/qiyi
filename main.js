6abde404934e7edd8b6f7c2ddf7ad19702759438
Abdulaziz780 <akhusanov410@gmail.com>
2024-05-10 22:06:06 +0500

    qiying

---
+++ main.js
@@ -0,0 +1,10 @@
+// const data = [34, 67, 49, 20, 23, 56, 90, 1000, 78, 54, 50];
+
+//     const dataList = document.getElementById('data');
+
+//     const html = data.map(number => {
+//       let colorClass = number > 50 ? 'green' : 'red';
+//       return `<li class="${colorClass}">${number}</li>`;
+//     }).join('');
+
+//     dataList.innerHTML = html;
